81ff3577e2ac7c8981caf32aeff6db88cf652093
Belov <bid97@mail.ru>
2024-08-06 22:37:44 +0300

    working on progect

--- Form.js
+++ Form.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll('button');
 for(let button of buttons){
-    button.addEventListener('click', function (e) {
+    button.addEventListener('click', function rerov(e) {
         e.preventDefault();
 
     const form = this.parentNode;
@@ -21,6 +21,13 @@ const SumEmail = document.getElementById('SumEmail');
 const SumExperience = document.getElementById('SumExperience');
 const SumDesign = document.getElementById('SumDesign');
 const ConfButton = document.getElementById('ConfButton');
+const point = document.querySelectorAll('point');
+const active = document.querySelector('active');
+const pointOne = document.getElementById('pointOne');
+const pointTwo = document.getElementById('pointTwo');
+const pointThree = document.getElementById('pointThree');
+
+
 
 ConfButton.addEventListener('click',function(e) {
     alert("Спасибо");
@@ -31,4 +38,47 @@ inputEmail.addEventListener('input', () => SumEmail.value = inputEmail.value);
 inputExperiemce.addEventListener('input', () => SumExperience.value = inputExperiemce.value);
 inputDesign.addEventListener('input', () => SumDesign.value = inputDesign.value);
 
+let index = 0;
 
+  function UpdateTabColor(NewIndex) {
+    for (let i = 0; i < 3; i++) {
+      if (i == NewIndex) {
+        point[i].querySelectorall('.point').className = "point active";
+      } else {
+        point[i].querySelectorall('.point').className = "point";
+      }
+    }
+  }
+
+
+  function UpdateTabColor(NewIndex) {
+    for (let i = 0; i < 3; i++) {
+      if (i == NewIndex) {
+        listItemElement[i].querySelector('.item__link').className = "item__link item__color";
+      } else {
+        listItemElement[i].querySelector('.item__link').className = "item__link";
+      }
+    }
+  }
+
+
+
+  pointOne.onclick = function (e) {
+    index = 0;
+    UpdateTabColor(index);
+  
+  }
+
+  pointTwo.onclick = function (e) {
+    index = 1;
+    UpdateTabColor(index);
+  
+  }
+  
+  // нажатие на правую  точку
+  
+  pointThree.onclick = function (e) {
+    index = 2;
+    UpdateTabColor(index);
+  
+  }
