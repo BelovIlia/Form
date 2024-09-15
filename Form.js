51d680cee7401a10f98a670d889a39e854876a9a
Belov <bid97@mail.ru>
2024-09-15 22:21:07 +0300

    adjustments

--- Form.js
+++ Form.js
@@ -9,31 +9,43 @@ function UpdateTabColor(NewIndex) {
   }
 }
 
+function changeChoosedValues(values) {
+  labelForm.innerHTML = "";
+  if (values.length === 0) {
+    labelForm.innerHTML = "<li>Ничего не выбрано</li>";
+    return;
+  }
+
+  for (const value of values) {
+    labelForm.innerHTML = `${labelForm.innerHTML}<li>${value}</li>`;
+  }
+}
 const inputName = document.getElementById('name');
 const inputEmail = document.getElementById('email');
 const inputExperiemce = document.getElementById('experience');
 const inputDesign = document.getElementById('design');
-const SumName = document.getElementById('SumName');
-const SumEmail = document.getElementById('SumEmail');
-const SumExperience = document.getElementById('SumExperience');
-const SumDesign = document.getElementById('SumDesign');
-const ConfButton = document.getElementById('ConfButton');
+const sumName = document.getElementById('sumName');
+const sumEmail = document.getElementById('sumEmail');
+const sumExperience = document.getElementById('sumExperience');
+const sumDesign = document.getElementById('sumDesign');
+const confButton = document.getElementById('confButton');
 const point = document.querySelectorAll('.point');
 const active = document.querySelector('.active');
 const pointOne = document.getElementById('pointOne');
 const pointTwo = document.getElementById('pointTwo');
 const pointThree = document.getElementById('pointThree');
 const form = document.querySelectorAll('form');
-const ContButton1 = document.getElementById('ContButton1');
-const ContButton2 = document.getElementById('ContButton2');
-const LabelForm = document.querySelector('.elem__form-topic');
+const contButton1 = document.getElementById('contButton1');
+const contButton2 = document.getElementById('contButton2');
+const labelForm = document.querySelector('.elem__form-topic');
 const checkboxes = document.querySelectorAll(".custom-checkbox > input");
+const step = document.querySelector('.step');
 
-ConfButton.addEventListener('click', function (e) {
+confButton.addEventListener('click', function (e) {
   alert("Спасибо");
 });
 
-ContButton1.addEventListener('click', function (e){
+contButton1.addEventListener('click', function (e){
   e.preventDefault();
   const hasAt = inputEmail.value.includes('@');
   const errors = [];
@@ -61,58 +73,42 @@ ContButton1.addEventListener('click', function (e){
   UpdateTabColor(index);
 })
 
-inputName.addEventListener('input', () => SumName.value = inputName.value);
-inputEmail.addEventListener('input', () => SumEmail.value = inputEmail.value);
+inputName.addEventListener('input', () => sumName.value = inputName.value);
+inputEmail.addEventListener('input', () => sumEmail.value = inputEmail.value);
 
 
 
-pointOne.addEventListener('click', function (e) {
-  for(i = 0;  i < form.length ; i++){
-    form[i].style = 'display: none';
-  }
-  const nextForm = document.querySelector(`form[data-step="${1}"]`);
-  nextForm.style = 'display: block;';
-  index = 0;
-  UpdateTabColor(index);
-});
+// pointOne.addEventListener('click', function (e) {
+//   for(i = 0;  i < form.length ; i++){
+//     form[i].style = 'display: none';
+//   }
+//   const nextForm = document.querySelector(`form[data-step="${1}"]`);
+//   nextForm.style = 'display: block;';
+//   index = 0;
+//   UpdateTabColor(index);
+// });
 
-pointTwo.addEventListener('click', function (e) {
-  for(i = 0;  i < form.length ; i++){
-    form[i].style = 'display: none';
-  }
-  const nextForm = document.querySelector(`form[data-step="${2}"]`);
-  nextForm.style = 'display: block;'
-  index = 1;
-  UpdateTabColor(index);
-});
+// pointTwo.addEventListener('click', function (e) {
+//   for(i = 0;  i < form.length ; i++){
+//     form[i].style = 'display: none';
+//   }
+//   const nextForm = document.querySelector(`form[data-step="${2}"]`);
+//   nextForm.style = 'display: block;'
+//   index = 1;
+//   UpdateTabColor(index);
+// });
 
-pointThree.addEventListener('click', function (e) {
-  for(i = 0;  i < form.length ; i++){
-    form[i].style = 'display: none';
-  }
-  const nextForm = document.querySelector(`form[data-step="${3}"]`);
-  nextForm.style = 'display: block;';
-  index = 2;
-  UpdateTabColor(index);
-});
+// pointThree.addEventListener('click', function (e) {
+//   for(i = 0;  i < form.length ; i++){
+//     form[i].style = 'display: none';
+//   }
+//   const nextForm = document.querySelector(`form[data-step="${3}"]`);
+//   nextForm.style = 'display: block;';
+//   index = 2;
+//   UpdateTabColor(index);
+// });
 
-
-
-const Step = document.querySelector('.Step');
-
-function changeChoosedValues(values) {
-  LabelForm.innerHTML = "";
-  if (values.length === 0) {
-    LabelForm.innerHTML = "<li>Ничего не выбрано</li>";
-    return;
-  }
-
-  for (const value of values) {
-    LabelForm.innerHTML = `${LabelForm.innerHTML}<li>${value}</li>`;
-  }
-}
-
-ContButton2.addEventListener('click', function (e){
+contButton2.addEventListener('click', function (e){
   e.preventDefault();
   const choosedValues = [];
   for (const checkbox of checkboxes) {
